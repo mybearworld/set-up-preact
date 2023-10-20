88dbad19297f439a55bedf7b89d0c2b378f053c7
mybearworld <130385691+mybearworld@users.noreply.github.com>
2023-10-20 16:48:43 +0200

    Create fonts folder and add Inter to it

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,13 @@ use std::fs;
 mod files;
 
 fn main() {
+  let inter = include_bytes!("../fonts/Inter.var.woff2");
+  fs::create_dir("src/assets/fonts")
+    .unwrap_or_else(|err| eprintln!("Note: Failed to create src/assets/fonts: {err}"));
+  fs::write("src/assets/fonts/Inter.var.woff2", inter).unwrap_or_else(|err| {
+    eprintln!("Note: Failed to create src/assets/fonts/Inter.var.woff2: {err}")
+  });
+
   for [path, contents] in files::files() {
     fs::write(path, contents)
       .unwrap_or_else(|err| eprintln!("Note: Failed to write {path}: {err}"));
